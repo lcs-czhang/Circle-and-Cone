393558e51c82ba115771bf2d3be6e6b76595c436
Youzhou Zhang <czhang@lcs.on.ca>
2024-01-22 08:46:27 -0500

    Initial Commit

---
+++ Circle and Cone/Circle_and_ConeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Circle_and_ConeApp.swift
+//  Circle and Cone
+//
+//  Created by Yuzhou Zhang on 2024-01-22.
+//
+
+import SwiftUI
+
+@main
+struct Circle_and_ConeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
